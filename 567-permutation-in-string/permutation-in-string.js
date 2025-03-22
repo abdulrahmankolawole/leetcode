a1607b48b8243aeb286cb29809dbda3c8588c8ae
Abdulrahman Kolawole <backendkolawole@gmail.com>
2025-03-22 12:32:38 +0100

    Time: 155 ms (18.53%) | Memory: 68.1 MB (5.19%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.js
+++ 567-permutation-in-string/permutation-in-string.js
@@ -1,27 +1,21 @@
-// declare two lookup objects
-// count the frequencies of char in s1 and the first s1.length characters in s2
-// if the two objects are identical, return true
-// iterate through string2 making the comparisons while we are still in bounds
-// return false after the iteration 
-
 var checkInclusion = function(s1, s2) {
     const _ = require('lodash');
 
-    let lookup1 = {}
-    let lookup2 = {}
+    let lookup1 = {};
+    let lookup2 = {};
 
-    if (s1.length > s2.length) return false
+    if (s1.length > s2.length) return false;
 
     for (let i = 0; i < s1.length; i++) {
-        let curr = s1[i]
-        if (!(curr in lookup1)) lookup1[curr] = 0
-        lookup1[curr] += 1
+        let curr = s1[i];
+        if (!(curr in lookup1)) lookup1[curr] = 0;
+        lookup1[curr] += 1;
     }   
 
     for (let i = 0; i < s1.length; i++) {
-        let curr = s2[i]
-        if (!(curr in lookup2)) lookup2[curr] = 0
-        lookup2[curr] += 1
+        let curr = s2[i];
+        if (!(curr in lookup2)) lookup2[curr] = 0;
+        lookup2[curr] += 1;
     }
 
     // Compare initial counts
@@ -29,29 +23,30 @@ var checkInclusion = function(s1, s2) {
         return true;
     }
 
-    let i = 0
-    let j = s1.length
+    let i = 0;
+    let j = s1.length;
 
     while (j < s2.length) {
-        let curr = s2[j]
+        let curr = s2[j];
 
-        if (!(curr in lookup2)) lookup2[curr] = 0
-        lookup2[curr] += 1
+        if (!(curr in lookup2)) lookup2[curr] = 0;
+        lookup2[curr] += 1;
 
-        if (lookup2[s2[i]] == 1) {
-            delete lookup2[s2[i]]
+        // Remove the character that is sliding out of the window
+        if (lookup2[s2[i]] === 1) {
+            delete lookup2[s2[i]];
+        } else {
+            lookup2[s2[i]] -= 1;
         }
-        else lookup2[s2[i]] -= 1
 
-        i += 1
-        // Compare initial counts
+        i += 1;
+        j += 1;
+
+        // Compare counts after updating
         if (_.isEqual(lookup1, lookup2)) {
             return true;
         }
-
-        j += 1
     }
 
-    return false
-    
+    return false;
 };
