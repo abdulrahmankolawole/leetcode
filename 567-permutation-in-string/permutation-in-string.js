43f634d4cde7cb655ca466009ee02710f04a42d0
Abdulrahman Kolawole <backendkolawole@gmail.com>
2025-03-22 12:32:15 +0100

    Time: 155 ms (18.53%) | Memory: 68.7 MB (5.19%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.js
+++ 567-permutation-in-string/permutation-in-string.js
@@ -1,38 +1,57 @@
+// declare two lookup objects
+// count the frequencies of char in s1 and the first s1.length characters in s2
+// if the two objects are identical, return true
+// iterate through string2 making the comparisons while we are still in bounds
+// return false after the iteration 
+
 var checkInclusion = function(s1, s2) {
     const _ = require('lodash');
-    if (s1.length > s2.length) return false;
 
-    let obj1 = {};
-    let obj2 = {};
+    let lookup1 = {}
+    let lookup2 = {}
 
-    // Populate obj1
+    if (s1.length > s2.length) return false
+
     for (let i = 0; i < s1.length; i++) {
-        obj1[s1[i]] = (obj1[s1[i]] || 0) + 1;
-        obj2[s2[i]] = (obj2[s2[i]] || 0) + 1;
+        let curr = s1[i]
+        if (!(curr in lookup1)) lookup1[curr] = 0
+        lookup1[curr] += 1
+    }   
+
+    for (let i = 0; i < s1.length; i++) {
+        let curr = s2[i]
+        if (!(curr in lookup2)) lookup2[curr] = 0
+        lookup2[curr] += 1
     }
 
     // Compare initial counts
-    if (_.isEqual(obj1, obj2)) {
+    if (_.isEqual(lookup1, lookup2)) {
         return true;
     }
 
-    let i = 0;
-    for (let j = s1.length; j < s2.length; j++) {
-        // Add the next character in s2
-        obj2[s2[j]] = (obj2[s2[j]] || 0) + 1;
+    let i = 0
+    let j = s1.length
 
-        // Remove the oldest character from s2
-        if (obj2[s2[i]] === 1) {
-            delete obj2[s2[i]];
+    while (j < s2.length) {
+        let curr = s2[j]
+
+        if (!(curr in lookup2)) lookup2[curr] = 0
+        lookup2[curr] += 1
+
+        if (lookup2[s2[i]] == 1) {
+            delete lookup2[s2[i]]
         }
-        else obj2[s2[i]] -= 1;
+        else lookup2[s2[i]] -= 1
 
-        i += 1;
-
-        if (_.isEqual(obj1, obj2)) {
-        return true;
+        i += 1
+        // Compare initial counts
+        if (_.isEqual(lookup1, lookup2)) {
+            return true;
         }
+
+        j += 1
     }
 
-    return false;
+    return false
+    
 };
