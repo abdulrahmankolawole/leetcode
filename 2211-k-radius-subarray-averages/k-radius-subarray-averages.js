090ecac31e8116611d038799ef973d730c7df2da
Abdulrahman Kolawole <backendkolawole@gmail.com>
2025-03-23 11:48:14 +0100

    Time: 24 ms (81.85%) | Memory: 87.7 MB (57.23%) - LeetSync

--- 2211-k-radius-subarray-averages/k-radius-subarray-averages.js
+++ 2211-k-radius-subarray-averages/k-radius-subarray-averages.js
@@ -1,24 +1,23 @@
 var getAverages = function(nums, k) {
-    let windowSize = (2 * k) + 1
-    let output = new Array(nums.length)
+    let windowSize = (2 * k ) + 1
+    let output = new Array(nums.length).fill(-1)
     let windowSum = 0
 
     for (let i = 0; i < windowSize; i++) {
         windowSum += nums[i]
-    }
+    }   
 
     for (let i = 0; i < nums.length; i++) {
-        if (i < k || i > nums.length- 1 - k) output[i] = -1
+        if (i < k || i > nums.length - 1 - k) continue
         else if (i == k) {
             output[i] = Math.trunc(windowSum / windowSize)
         }
         else {
             windowSum += nums[i + k] - nums[i - k - 1]
-            output[i] = Math.trunc(windowSum / windowSize )
-        } 
+            output[i] = Math.trunc(windowSum / windowSize)
+        }
     }
 
-    return output
 
-    
+    return output
 };
