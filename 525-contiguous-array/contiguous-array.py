a2a098fbbd74d2ba134bbc02fbb3112cf9379ddf
Abdulrahman Kolawole <backendkolawole@gmail.com>
2025-03-27 15:19:52 +0100

    Time: 107 ms (43.66%) | Memory: 23.5 MB (50.24%) - LeetSync

--- 525-contiguous-array/contiguous-array.py
+++ 525-contiguous-array/contiguous-array.py
@@ -1,19 +1,19 @@
 class Solution:
     def findMaxLength(self, nums: List[int]) -> int:
         lookup = {0: -1}
-        max_length = 0
         count = 0
+        output = 0
 
-        for i, val in enumerate(nums):
-            if (val == 0):
+        for i in range(len(nums)):
+            if (nums[i] == 0):
                 count -= 1
-            else:
+            elif (nums[i] == 1):
                 count += 1
             
             if (count not in lookup):
                 lookup[count] = i
             else:
-                max_length = max(max_length, i - lookup[count])
-        
-        return max_length
-        
+                length = i - lookup[count]
+                output = max(length, output)
+
+        return output
