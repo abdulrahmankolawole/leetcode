fedeaa2916dd4e938052cd9fd2c5c5b792705c90
Abdulrahman Kolawole <backendkolawole@gmail.com>
2025-03-27 15:29:12 +0100

    Time: 39 ms (36.92%) | Memory: 20.5 MB (36.97%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.py
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.py
@@ -1,7 +1,7 @@
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
         lookup = {0: 1}
-        output = 0
+        count = 0
         total = 0
 
         for i, val in enumerate(nums):
@@ -9,13 +9,13 @@ class Solution:
             prefix = total - k
 
             if (prefix in lookup):
-                output += lookup[prefix]
+                count += lookup[prefix]
             
             if (total not in lookup):
                 lookup[total] = 0
             lookup[total] += 1
 
-        return output
-
-
         
+        return count
+
+            
