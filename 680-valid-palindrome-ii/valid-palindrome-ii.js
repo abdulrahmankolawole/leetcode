19a8856f219a4bcfc6003cec494e163a2c104f8d
Abdulrahman Kolawole <backendkolawole@gmail.com>
2025-03-31 11:06:51 +0100

    Time: 29 ms (7.31%) | Memory: 67.5 MB (5.12%) - LeetSync

---
+++ 680-valid-palindrome-ii/valid-palindrome-ii.js
@@ -0,0 +1,29 @@
+var validPalindrome = function(s) {
+    let i = 0
+    let j = s.length - 1
+
+    function isPalindrome(word) {
+        i = 0
+        j = word.length - 1
+
+        while (i < j) {
+            if (word[i] != word[j]) return false
+            i += 1
+            j -= 1
+        }
+        return true
+    }   
+
+    while (i <= j) {
+        if (s[i] != s[j]) {
+            word1 = s.slice(0, i) + s.slice(i + 1)
+            word2 = s.slice(0, j) + s.slice(j + 1)
+            console.log(word1, word2)
+            if (!isPalindrome(word1) && !isPalindrome(word2)) return false
+        }
+        i += 1
+        j -= 1
+    }
+    return true
+
+};
