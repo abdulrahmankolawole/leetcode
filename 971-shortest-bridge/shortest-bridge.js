98ce8d302a253ac81b76ca673e5eac4762abfd75
Abdulrahman Kolawole <backendkolawole@gmail.com>
2025-05-12 15:20:02 +0100

    Time: 56 ms (38.17%) | Memory: 64 MB (64.88%) - LeetSync

---
+++ 971-shortest-bridge/shortest-bridge.js
@@ -0,0 +1,59 @@
+var shortestBridge = function(grid) {
+    let directions = [[-1, 0], [0, 1], [1, 0], [0, -1]]
+    let n = grid.length
+
+    function isValid(i, j) {
+        if (i < 0 || j < 0 || i == n || j == n || mySet.has(i + ',' + j)) {
+            return false
+        }
+        return true
+
+    }
+
+    let queue = []
+    let mySet = new Set()
+    
+    function traverse(i, j) {
+        if (!isValid(i, j) || grid[i][j] != 1) return 
+        grid[i][j] = 0
+        queue.push([i, j])
+        traverse(i - 1, j) // up
+        traverse(i, j + 1) // right
+        traverse(i + 1, j) // down
+        traverse(i, j - 1) // left
+    }
+
+    function bfs() {
+        let bridge = 0
+        while (queue.length) {
+            let size = queue.length
+
+            for (let i = 0; i < size; i++) {
+                let [i, j] = queue.shift()
+
+
+                for (let [x, y] of directions) {
+                    new_x = x + i
+                    new_y = y + j
+                    if (isValid(new_x, new_y)) {
+                        if (grid[new_x][new_y] == 1) return bridge
+                        queue.push([new_x, new_y])
+                        mySet.add(new_x + "," + new_y)
+                    }
+                }
+
+            }
+            bridge += 1
+        }
+    }
+
+    for (let i = 0; i < n; i++) {
+        for (let j = 0; j < n; j++) {
+            let cell = grid[i][j]
+            if (cell == 1) {
+                traverse(i, j)
+                return bfs()
+            }
+        }
+    }
+};
