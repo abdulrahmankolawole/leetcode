d475930cf88647b2cdc053255badca89d969e6fb
Abdulrahman Kolawole <backendkolawole@gmail.com>
2025-05-13 14:48:37 +0100

    Time: 5 ms (80.17%) | Memory: 56.2 MB (82.64%) - LeetSync

---
+++ 1380-number-of-closed-islands/number-of-closed-islands.js
@@ -0,0 +1,38 @@
+var closedIsland = function(grid) {
+    let rows = grid.length
+    let columns = grid[0].length
+    let count = 0
+
+    function traverse(i, j) {
+        if (i < 0 || j < 0 || i == rows || j == columns || grid[i][j] != 0) return
+        grid[i][j] = 1
+        traverse(i - 1, j) // up
+        traverse(i, j + 1) // right
+        traverse(i + 1, j) // down
+        traverse(i, j - 1) // left
+
+    }
+
+    for (let i = 0; i < rows; i++) {
+        for (let j = 0; j < columns; j++) {
+            let cell = grid[i][j]
+            if (cell == 0) {
+                if (i == 0 || j == 0 || i == rows - 1 || j == columns - 1) traverse(i, j)
+
+            }
+        }
+    }  
+
+    for (let i = 0; i < rows; i++) {
+        for (let j = 0; j < columns; j++) {
+            let cell = grid[i][j]
+            if (cell == 0) {
+                count += 1
+                traverse(i, j)
+
+            }
+        }
+    }   
+
+    return count 
+};
