03f1b8ebb8e2b08b5fb90a91a895db64b6fd1049
Abdulrahman Kolawole <backendkolawole@gmail.com>
2025-05-21 13:13:32 +0100

    Time: 39 ms (90.15%) | Memory: 22.6 MB (32.30%) - LeetSync

--- 1320-remove-all-adjacent-duplicates-in-string-ii/remove-all-adjacent-duplicates-in-string-ii.py
+++ 1320-remove-all-adjacent-duplicates-in-string-ii/remove-all-adjacent-duplicates-in-string-ii.py
@@ -7,23 +7,19 @@ class Solution:
                 stack.append([val, 1])
                 continue
 
-            else:
-                if (stack[-1][0] != val):
-                    stack.append([val, 1])
-                else:
-                    streak = stack[-1][1]
+            if (stack[-1][0] != val):
+                stack.append([val, 1])
+                continue
 
-                    if (streak + 1 == k):
-                        stack.pop()
-                        continue
-                    
-                    stack[-1][1] = streak + 1
+            streak = stack[-1][1]
+            if (streak + 1 == k):
+                stack.pop()
+                continue
+            
+            stack[-1][1] = streak + 1
                 
 
-        result = []
-
-        for val, count in stack:
-            result.append(val * count)
+        result = [(val * count) for val, count in stack]
 
         return "".join(result)
         
