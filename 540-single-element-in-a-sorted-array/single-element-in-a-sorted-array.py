f7b38f9b317cb79fc0af762f311cf6612d4ad701
Abdulrahman Kolawole <backendkolawole@gmail.com>
2025-05-22 11:16:13 +0100

    Time: 0 ms (100.00%) | Memory: 24.7 MB (97.01%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.py
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.py
@@ -5,9 +5,6 @@ class Solution:
 
         while (i <= j):
             mid = (i + j) // 2
-
-            # if (nums[mid - 1] != nums[mid] and nums[mid] != nums[mid + 1] ):
-            #     return nums[mid]
             if ((mid == 0 or nums[mid - 1] != nums[mid]) and( mid == len(nums) -1 or nums[mid] != nums[mid + 1]) ):
                 return nums[mid]
             left_size = mid - 1 if nums[mid - 1] == nums[mid] else mid
