63025db7fd67a043575fb0733a3b8d4bf16c4e32
Abdulrahman Kolawole <backendkolawole@gmail.com>
2025-05-23 08:09:48 +0100

    Time: 44 ms (24.71%) | Memory: 72.4 MB (22.43%) - LeetSync

--- 187-repeated-dna-sequences/repeated-dna-sequences.js
+++ 187-repeated-dna-sequences/repeated-dna-sequences.js
@@ -1,19 +1,18 @@
 var findRepeatedDnaSequences = function(s) {
-    let lookup = {}
-    let i = 0
-    let j = 0
     let output = []
+    if (s.length < 10) return output
+    let lookup = {}
+    let j = 0
 
     while (j < s.length - 9) {
-        let substring = s.substring(j, j + 10)
-
-        if (!(substring in lookup)) lookup[substring] = 0
-        lookup[substring] += 1   
-
-        if (lookup[substring] == 2) {
-            output.push(substring)
-        }
+        let substr = s.slice(j, j + 10)
+        
+        if (!(substr in lookup)) lookup[substr] = 0
+        lookup[substr] += 1
+        if (lookup[substr] == 2) output.push(substr) 
         j += 1
     }
     return output
+
+
 };
