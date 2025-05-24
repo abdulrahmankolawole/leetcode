12898f1355f315722d553cdf0fe4956ad5c247d3
Abdulrahman Kolawole <backendkolawole@gmail.com>
2025-05-24 14:01:24 +0100

    Time: 29 ms (31.72%) | Memory: 18 MB (62.19%) - LeetSync

--- 1321-get-equal-substrings-within-budget/get-equal-substrings-within-budget.py
+++ 1321-get-equal-substrings-within-budget/get-equal-substrings-within-budget.py
@@ -3,20 +3,17 @@ class Solution:
         i = 0
         j = 0
         current_cost = 0
-        max_length = 0
+        output = 0
 
         while (j < len(s)):
-            ascii1 = ord(s[j])
-            ascii2 = ord(t[j])
-
-            current_cost += abs(ascii1 - ascii2)
+            current_cost += abs(ord(s[j]) - ord(t[j]))
 
             while (current_cost > maxCost):
                 current_cost -= abs(ord(s[i]) - ord(t[i]))
                 i += 1
             
-            max_length = max(max_length, j - i + 1)
+            output = max(output, j - i + 1)
             j += 1
 
-        return max_length
+        return output
         
