9d00952f725291ae13e9358dada93e9499f2ec2d
Abdulrahman Kolawole <backendkolawole@gmail.com>
2025-05-25 04:53:32 +0100

    Time: 129 ms (16.85%) | Memory: 69.8 MB (72.47%) - LeetSync

--- 1966-frequency-of-the-most-frequent-element/frequency-of-the-most-frequent-element.js
+++ 1966-frequency-of-the-most-frequent-element/frequency-of-the-most-frequent-element.js
@@ -1,21 +1,19 @@
 var maxFrequency = function(nums, k) {
-    nums = nums.sort((a, b) => a - b)   
+    nums = nums.sort((a, b)=> a - b)
     let i = 0
     let j = 0
-    let total = 0
     let output = 0
+    let currentSum = 0
 
     while (j < nums.length) {
-        total += nums[j]
+        currentSum += nums[j]
 
-        while (nums[j] * (j - i + 1) > total + k) {
-            total -= nums[i]
+        while (nums[j] * (j - i + 1) > currentSum + k) {
+            currentSum -= nums[i]
             i += 1
         }
-
-        output = Math.max(output, j - i + 1)
-        j += 1
+        output = Math.max(j - i + 1, output)
+        j +=1
     }
-
     return output
 };
