985a14dd071a190a05b263cfa80b8d1831b246a8
Abdulrahman Kolawole <backendkolawole@gmail.com>
2025-05-26 11:53:26 +0100

    Time: 353 ms (5.04%) | Memory: 21.3 MB (74.78%) - LeetSync

--- 2255-minimum-swaps-to-group-all-1s-together-ii/minimum-swaps-to-group-all-1s-together-ii.py
+++ 2255-minimum-swaps-to-group-all-1s-together-ii/minimum-swaps-to-group-all-1s-together-ii.py
@@ -1,23 +1,22 @@
 class Solution:
     def minSwaps(self, nums: List[int]) -> int:
-        total_no_of_ones = nums.count(1)
-        window_ones = 0
-        max_window_ones = 0
+        total_ones = nums.count(1)
         i = 0
         j = 0
+        max_window_ones = 0
+        window_ones = 0
 
         while (j < 2 * len(nums)):
-            if (nums[j % len(nums)]):
+            if (nums[j % len(nums)] == 1):
                 window_ones += 1
             
-            if (j - i + 1 > total_no_of_ones):
-                if (nums[i % len(nums)]):
+            while (j - i + 1 > total_ones):
+                if (nums[i % len(nums)] == 1):
                     window_ones -= 1
                 i += 1
-            
-            max_window_ones = max(max_window_ones, window_ones)
+
+            max_window_ones = max(window_ones, max_window_ones)
             j += 1
+
+        return total_ones - max_window_ones
         
-        return total_no_of_ones - max_window_ones
-                
-                
