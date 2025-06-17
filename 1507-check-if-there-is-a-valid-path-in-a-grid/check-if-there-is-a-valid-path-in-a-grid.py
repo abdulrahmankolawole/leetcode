225f32c94c357aea6cddf49bebb4837fe7391bf3
Abdulrahman Kolawole <backendkolawole@gmail.com>
2025-06-17 13:31:06 +0100

    Time: 375 ms (35.28%) | Memory: 33.7 MB (74.93%) - LeetSync

--- 1507-check-if-there-is-a-valid-path-in-a-grid/check-if-there-is-a-valid-path-in-a-grid.py
+++ 1507-check-if-there-is-a-valid-path-in-a-grid/check-if-there-is-a-valid-path-in-a-grid.py
@@ -1,52 +1,39 @@
 class Solution:
     def hasValidPath(self, grid: List[List[int]]) -> bool:
-        rows = len(grid)
-        columns = len(grid[0])
-        visited = set()
-        
-        # dictionaries for transition rule
         up = {x: set([2, 3, 4]) for x in [2, 5, 6]}
         right = {x: set([1, 3, 5]) for x in [1, 4, 6]}
         down = {x: set([2, 5, 6]) for x in [2, 3, 4]}
-        left = {x: set([1, 4, 6]) for x in [1, 3, 5] }
-        
-        def isValid(dr, dc, i, j, direction):
+        left = {x: set([1, 4, 6]) for x in [1, 3, 5]}
+        rows = len(grid)
+        columns = len(grid[0])
+        lookup = set()
 
-            if dr < 0 or dr == rows or dc < 0 or dc == columns or (dr, dc) in visited:
-                return False
-
+        def isValid(i, j, row, col, direction):
+            if (row < 0 or col < 0 or row == rows or col == columns or (row, col) in lookup):
+                return False            
             street = grid[i][j]
-
-            if street not in direction:
+            if (street not in direction):
                 return False
-
-            path = grid[dr][dc]
-            if path not in direction[street]:
+            path = grid[row][col]
+            if (path not in direction[street]):
                 return False
-
             return True
-        
-
-        directions = [(-1, 0, up), 
-                        (0, 1, right), 
-                        (1, 0, down),
-                        (0, -1, left) ]
 
         queue = [(0, 0)]
-
-        while queue:
-
-            i, j = queue.pop(0)
-            
-            if (i, j) == (rows - 1, columns - 1):
+        directions = [(-1, 0, up), (0, 1, right), (1, 0, down), (0, -1, left)]
+        
+        while (queue):
+            x, y = queue.pop(0)
+            if (x == rows - 1 and y == columns - 1):
                 return True
-            for dr, dc, map in directions:
-                new_x = dr + i
-                new_y = dc + j
 
-                # if we travel out of bounds return False
-                if isValid(new_x, new_y, i, j, map):
-                    visited.add((new_x, new_y))
-                    queue.append((new_x, new_y))
+            for i, j, direction in directions:
+                row = i + x
+                col = j + y
+
+                if (isValid(x, y, row, col, direction)):
+                    queue.append((row, col))
+                    lookup.add((row, col))
 
         return False
+        
