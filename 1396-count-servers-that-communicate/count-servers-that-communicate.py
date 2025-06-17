4fcd172a447a43778342b188effd84004f0532b6
Abdulrahman Kolawole <backendkolawole@gmail.com>
2025-06-17 12:50:34 +0100

    Time: 21 ms (45.65%) | Memory: 19.7 MB (33.62%) - LeetSync

--- 1396-count-servers-that-communicate/count-servers-that-communicate.py
+++ 1396-count-servers-that-communicate/count-servers-that-communicate.py
@@ -1,35 +1,32 @@
 class Solution:
     def countServers(self, grid: List[List[int]]) -> int:
+        rowLookup = {}
+        columnLookup = {}
         rows = len(grid)
         columns = len(grid[0])
-        lookup = {}
-
-        for i in range(rows):
-            for j in range(columns):
-                cell = grid[i][j]
-
-                if (cell):
-                    row = f"row: {i}"
-                    column = f"column: {j}"
-
-                    if (row not in lookup):
-                        lookup[row] = 0
-                    lookup[row] += 1
-
-                    if (column not in lookup):
-                        lookup[column] = 0
-                    lookup[column] += 1
-
         count = 0
+
         for i in range(rows):
             for j in range(columns):
                 cell = grid[i][j]
-                row = f"row: {i}"
-                column = f"column: {j}"
 
                 if (cell):
-                    if (lookup[row] > 1 or lookup[column] > 1):
+                    if i not in rowLookup:
+                        rowLookup[i] = 0
+                    rowLookup[i] += 1
+
+                    if j not in columnLookup:
+                        columnLookup[j] = 0
+                    columnLookup[j] += 1
+
+        for i in range(rows):
+            for j in range(columns):
+                cell = grid[i][j]
+
+                if (cell):
+                    if rowLookup[i] > 1 or columnLookup[j] > 1:
                         count += 1
 
         return count
+                
         
