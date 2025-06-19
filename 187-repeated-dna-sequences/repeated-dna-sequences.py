9904dc9e352d7cbc3781330c37ab60a72ace15f1
Abdulrahman Kolawole <backendkolawole@gmail.com>
2025-06-19 13:52:10 +0100

    Time: 31 ms (31.54%) | Memory: 29 MB (59.19%) - LeetSync

--- 187-repeated-dna-sequences/repeated-dna-sequences.py
+++ 187-repeated-dna-sequences/repeated-dna-sequences.py
@@ -1,20 +1,19 @@
 class Solution:
     def findRepeatedDnaSequences(self, s: str) -> List[str]:
-        
+        i = 0
         lookup = {}
-        result = []
-        j = 0
+        output = []
 
-        while (j < len(s) - 9):
-            substring = s[j: j + 10]
+        while (i < len(s) - 9):
+            substring = s[i: i + 10]
 
             if (substring not in lookup):
                 lookup[substring] = 0
+
             lookup[substring] += 1
-
             if (lookup[substring] == 2):
-                result.append(substring)
+                output.append(substring)
+            i += 1
+        
 
-            j +=1
-
-        return result 
+        return output
