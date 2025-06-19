85dd691f132ef13c13fa4a3a8d85a488dc60c00d
Abdulrahman Kolawole <backendkolawole@gmail.com>
2025-06-19 15:16:42 +0100

    Time: 57 ms (10.16%) | Memory: 55 MB (90.82%) - LeetSync

--- 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.js
+++ 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.js
@@ -1,28 +1,27 @@
 var numberOfSubstrings = function(s) {
-
     let lookup = {}
 
     function containsAllThree() {
-        return (lookup["a"] > 0 && lookup["b"] > 0 && lookup["c"] > 0)
-    }
+        if (!("a" in lookup) || !("b" in lookup) || !("c" in lookup)) return false
+        return lookup["a"] > 0 && lookup["b"] > 0 && lookup["c"] > 0
+    }    
 
     let i = 0
     let j = 0
     let output = 0
-    
+
     while (j < s.length) {
-        if (!(s[j] in lookup)) lookup[s[j]] = 0
-        lookup[s[j]] += 1
+        let char = s[j]
+        if (!(char in lookup)) lookup[char] = 0
+        lookup[char] += 1
 
         while (containsAllThree()) {
-            output += s.length - j
             lookup[s[i]] -= 1
             i += 1
+            output += s.length - j
         }
-        j +=1 
+        j += 1
     }
 
     return output
-
-
 };
