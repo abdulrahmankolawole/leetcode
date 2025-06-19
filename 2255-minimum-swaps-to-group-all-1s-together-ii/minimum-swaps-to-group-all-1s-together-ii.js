8345bf86038f147d230d7dc55152d8cbd3494989
Abdulrahman Kolawole <backendkolawole@gmail.com>
2025-06-19 15:33:04 +0100

    Time: 14 ms (40.51%) | Memory: 64.2 MB (74.68%) - LeetSync

--- 2255-minimum-swaps-to-group-all-1s-together-ii/minimum-swaps-to-group-all-1s-together-ii.js
+++ 2255-minimum-swaps-to-group-all-1s-together-ii/minimum-swaps-to-group-all-1s-together-ii.js
@@ -1,27 +1,26 @@
 var minSwaps = function(nums) {
-    let totalOnes = 0
-    let windowOnes = 0
-    let maxWindowOnes = 0
+    let countOfOnes = 0
+    let k = nums.length
+
+    for (let num of nums) {
+        countOfOnes += num
+    }
+    
     let i = 0
     let j = 0
-    let n = nums.length
+    let maxWindowOnes = 0
+    let windowOnes = 0
 
-    for (let i = 0; i < n; i++) {
-        if (nums[i] == 1) totalOnes += 1
-    }
+    while (j < nums.length * 2) {
+        if (nums[j % k]) windowOnes += 1
 
-    while (j < 2 * n) {
-        if (nums[j % n]) windowOnes += 1
-
-        while (j - i + 1 > totalOnes) {
-            if (nums[i % n]) windowOnes -= 1
-            i +=1
+        while (j - i + 1 > countOfOnes) {
+            if (nums[i % k]) windowOnes -= 1
+            i += 1
         }
-        
         maxWindowOnes = Math.max(maxWindowOnes, windowOnes)
-        j += 1
+        j += 1 
     }
-
-    return totalOnes - maxWindowOnes
+    return countOfOnes - maxWindowOnes
 
 };
