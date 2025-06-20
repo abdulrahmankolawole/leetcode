b4bd8d67de945d8519b270f0f7cebab312674acd
Abdulrahman Kolawole <backendkolawole@gmail.com>
2025-06-20 18:48:09 +0100

    Time: 613 ms (70.93%) | Memory: 32.8 MB (81.95%) - LeetSync

--- 1787-sum-of-absolute-differences-in-a-sorted-array/sum-of-absolute-differences-in-a-sorted-array.py
+++ 1787-sum-of-absolute-differences-in-a-sorted-array/sum-of-absolute-differences-in-a-sorted-array.py
@@ -1,17 +1,15 @@
 class Solution:
     def getSumAbsoluteDifferences(self, nums: List[int]) -> List[int]:
-        total_sum = sum(nums)
+        total = sum(nums)
         left_sum = 0
-        result = [0 for _ in range(len(nums))]
+        output = [0 for _ in range(len(nums))]
 
-        for i, val in enumerate(nums):
-            right_sum = total_sum - val - left_sum
-            left_size = i
+        for idx, val in enumerate(nums):
+            right_sum = total - val - left_sum
+            left_size = idx
             right_size = len(nums) - left_size - 1
-
-            result[i] = ((left_size * val) - left_sum) + (right_sum - (right_size * val))
+            output[idx] = ((left_size * val) - left_sum) + (right_sum - (right_size * val))
             left_sum += val
-        
-        return result
 
+        return output
         
