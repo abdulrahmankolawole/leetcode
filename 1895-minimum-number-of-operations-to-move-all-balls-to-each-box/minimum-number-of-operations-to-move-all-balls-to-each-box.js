3e4420de19cbc4800de1739b836212f31c96b030
Abdulrahman Kolawole <backendkolawole@gmail.com>
2025-06-20 19:02:52 +0100

    Time: 3 ms (88.38%) | Memory: 57.9 MB (66.81%) - LeetSync

--- 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/minimum-number-of-operations-to-move-all-balls-to-each-box.js
+++ 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/minimum-number-of-operations-to-move-all-balls-to-each-box.js
@@ -1,20 +1,20 @@
 var minOperations = function(boxes) {
-    let result = new Array(boxes.length).fill(0)
+    let output = new Array(boxes.length).fill(0)
     let moves = 0
     let balls = 0
+
     for (let i = 0; i < boxes.length; i++) {
-        result[i] = balls + moves
+        output[i] = balls + moves
         moves = moves + balls
-        balls +=  parseInt(boxes[i])
-    }  
-
-    moves = 0
+        if (boxes[i] == "1") balls ++
+    }
     balls = 0
+    moves = 0
     for (let i = boxes.length - 1; i >= 0; i--) {
-        result[i] += balls + moves
+        output[i] += balls + moves
         moves = moves + balls
-        balls +=  parseInt(boxes[i])
-    }   
-    return result
+        if (boxes[i] == "1") balls ++
+    }
 
+    return output
 };
