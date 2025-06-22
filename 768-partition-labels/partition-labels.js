e80d2eb4faeafcc9ec953cd2564560c6f3b04152
Abdulrahman Kolawole <backendkolawole@gmail.com>
2025-06-22 11:37:30 +0100

    Time: 6 ms (51.14%) | Memory: 54.4 MB (77.64%) - LeetSync

--- 768-partition-labels/partition-labels.js
+++ 768-partition-labels/partition-labels.js
@@ -1,21 +1,23 @@
 var partitionLabels = function(s) {
-    let size = 0
-    let result = []
-    let end = 0
     let lookup = {}
 
     for (let i = 0; i < s.length; i++) {
-        lookup[s[i]] = i
-    }
+        let char = s[i]
+        lookup[char] = i
+    }    
+
+    let lastIdx = 0
+    let size = 0
+    let output = []
 
     for (let i = 0; i < s.length; i++) {
         size += 1
-        end = Math.max(end, lookup[s[i]])
-        if (i == end) {
-            result.push(size)
+        let char = s[i]
+        lastIdx = Math.max(lastIdx, lookup[char])
+        if (lastIdx == i) {
+            output.push(size)
             size = 0
-        } 
-    }   
-
-    return result
+        }
+    }
+    return output
 };
