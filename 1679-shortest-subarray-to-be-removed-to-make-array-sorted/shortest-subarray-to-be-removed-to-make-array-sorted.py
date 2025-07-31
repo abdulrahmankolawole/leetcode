8ba324a92768380b448da7bcdf0a18a483e03152
Abdulrahman Kolawole <backendkolawole@gmail.com>
2025-07-31 16:52:09 +0100

    Time: 13 ms (48.48%) | Memory: 32.9 MB (6.36%) - LeetSync

--- 1679-shortest-subarray-to-be-removed-to-make-array-sorted/shortest-subarray-to-be-removed-to-make-array-sorted.py
+++ 1679-shortest-subarray-to-be-removed-to-make-array-sorted/shortest-subarray-to-be-removed-to-make-array-sorted.py
@@ -1,36 +1,29 @@
 class Solution:
     def findLengthOfShortestSubarray(self, arr: List[int]) -> int:
         N = len(arr)
-        i = 0
-        j = N - 1
-        res = N - 1
+        l = 0
+        r = N - 1
+        res = N
+        while (l < N - 1 and arr[l] <= arr[l + 1]):
+            l += 1
 
-        # find the longest prefix you can remove to make array sorted
-        while (j > 0 and arr[j - 1] <= arr[j]):
-            j -= 1
+        while (r > 0 and arr[r] >= arr[r - 1]):
+            r -= 1
 
-        res = j
-        
-        # find the longest postfix you can remove to make array sorted
-        while (i + 1 < N  and arr[i] <= arr[i + 1]):
-            i += 1
-
-        if (i == N - 1):
+        if (r == 0):
             return 0
 
-        res = min(res, N - 1 - i)
-
-        # find smallest middle part you can remove
-        l = 0
-        r = j
-        while (l <= i and r < N):
-            if (arr[l] <= arr[r]):
-                res = min(res, r - l - 1)
-                l += 1
+        i = 0
+        j = r
+        res = min(res, r, N - l - 1)
+        while (i <= l and j < N):
+            if (arr[i] <= arr[j]):
+                res = min(res, j - i - 1)
+                i += 1
             else:
-                r += 1
+                j += 1
+
 
         return res
-
 
         
