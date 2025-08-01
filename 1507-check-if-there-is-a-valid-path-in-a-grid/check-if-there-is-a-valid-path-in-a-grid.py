ef4cf6ac26506870e7e63adcf161421fbe1b586d
Abdulrahman Kolawole <backendkolawole@gmail.com>
2025-08-01 15:22:36 +0100

    Time: 360 ms (21.84%) | Memory: 34 MB (75.08%) - LeetSync

--- 1507-check-if-there-is-a-valid-path-in-a-grid/check-if-there-is-a-valid-path-in-a-grid.py
+++ 1507-check-if-there-is-a-valid-path-in-a-grid/check-if-there-is-a-valid-path-in-a-grid.py
@@ -1,32 +1,34 @@
 class Solution:
     def hasValidPath(self, grid: List[List[int]]) -> bool:
+        queue = [(0, 0)]
+        rows = len(grid)
+        columns = len(grid[0])
         up = {x: set([2, 3, 4]) for x in [2, 5, 6]}
         right = {x: set([1, 3, 5]) for x in [1, 4, 6]}
         down = {x: set([2, 5, 6]) for x in [2, 3, 4]}
         left = {x: set([1, 4, 6]) for x in [1, 3, 5]}
-        rows = len(grid)
-        columns = len(grid[0])
+        directions = [(-1, 0, up), (0, 1, right), (1, 0, down), (0, -1, left)]
         lookup = set()
 
         def isValid(i, j, row, col, direction):
             if (row < 0 or col < 0 or row == rows or col == columns or (row, col) in lookup):
-                return False            
-            street = grid[i][j]
-            if (street not in direction):
+                return False
+            cell = grid[i][j]
+            if (cell not in direction):
                 return False
             path = grid[row][col]
-            if (path not in direction[street]):
+            if (path not in direction[cell]):
                 return False
-            return True
 
-        queue = [(0, 0)]
-        directions = [(-1, 0, up), (0, 1, right), (1, 0, down), (0, -1, left)]
+            return True
         
+
         while (queue):
             x, y = queue.pop(0)
+
             if (x == rows - 1 and y == columns - 1):
                 return True
-
+            
             for i, j, direction in directions:
                 row = i + x
                 col = j + y
@@ -36,4 +38,3 @@ class Solution:
                     lookup.add((row, col))
 
         return False
-        
