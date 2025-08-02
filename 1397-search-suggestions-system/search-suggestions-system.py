db9d8ab4381f58f266321486fa0dc4b548809d83
Abdulrahman Kolawole <backendkolawole@gmail.com>
2025-08-02 09:48:28 +0100

    Time: 3 ms (97.19%) | Memory: 20.5 MB (76.61%) - LeetSync

--- 1397-search-suggestions-system/search-suggestions-system.py
+++ 1397-search-suggestions-system/search-suggestions-system.py
@@ -1,23 +1,24 @@
 class Solution:
     def suggestedProducts(self, products: List[str], searchWord: str) -> List[List[str]]:
-        l = 0
-        r = len(products) - 1
         products.sort()
+        left = 0
+        right = len(products) - 1
         output = []
 
         for i in range(len(searchWord)):
             char = searchWord[i]
 
-            while (l <= r and (len(products[l]) <= i or products[l][i] != char)):
-                l += 1
-            while (l <= r and (len(products[r]) <= i or products[r][i] != char)):
-                r -= 1
+            while (left <= right and (len(products[left]) <= i or products[left][i] != char)):
+                left += 1
+            
+            while (left <= right and (len(products[right]) <= i or products[right][i] != char)):
+                right -= 1
 
+            distance = right - left + 1
+            
             output.append([])
-            distance = r - l + 1
-
-            for i in range(min(distance, 3)):
-                output[-1].append(products[l + i])
+            for k in range(min(3, distance)):
+                output[-1].append(products[k + left])
 
         return output
         
