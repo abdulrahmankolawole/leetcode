09c170f285e171d9bb90449aa80b465bc175d1aa
Abdulrahman Kolawole <backendkolawole@gmail.com>
2025-08-28 08:59:09 +0100

    Time: 27 ms (74.52%) | Memory: 18.3 MB (16.92%) - LeetSync

--- 1850-minimum-length-of-string-after-deleting-similar-ends/minimum-length-of-string-after-deleting-similar-ends.py
+++ 1850-minimum-length-of-string-after-deleting-similar-ends/minimum-length-of-string-after-deleting-similar-ends.py
@@ -2,15 +2,16 @@ class Solution:
     def minimumLength(self, s: str) -> int:
         i = 0
         j = len(s) - 1
-        
+
         while (i < j and s[i] == s[j]):
             char = s[i]
 
-            while (i <= j and char == s[i]):
+            while (i <= j and s[i] == char):
                 i += 1
 
-            while (i <= j and char == s[j]):
+            while (i <= j and s[j] == char):
                 j -= 1
-            
+
         return j - i + 1
+
         
