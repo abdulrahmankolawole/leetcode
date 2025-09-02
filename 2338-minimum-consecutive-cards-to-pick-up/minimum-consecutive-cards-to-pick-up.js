c5154b7428e69b5020628ac2ed6a2aed109f5e72
Abdulrahman Kolawole <backendkolawole@gmail.com>
2025-09-02 11:53:48 +0100

    Time: 130 ms (17.96%) | Memory: 87.5 MB (10.78%) - LeetSync

--- 2338-minimum-consecutive-cards-to-pick-up/minimum-consecutive-cards-to-pick-up.js
+++ 2338-minimum-consecutive-cards-to-pick-up/minimum-consecutive-cards-to-pick-up.js
@@ -1,16 +1,19 @@
 var minimumCardPickup = function(cards) {
+    let lookup = new Set()
     let output = Infinity
-    let lookup = {}
     let j = 0
 
     while (j < cards.length) {
         let card = cards[j]
 
         if (card in lookup) {
-            output = Math.min(output, j - lookup[card] + 1)
+            let size = j - lookup[card] + 1
+            output = Math.min(output, size)
         }
-        lookup[card] = j
-        j += 1
-    }  
-    return output == Infinity? - 1: output
+        
+        lookup[card] = j 
+        j ++ 
+    }    
+
+    return output == Infinity? -1 : output
 };
