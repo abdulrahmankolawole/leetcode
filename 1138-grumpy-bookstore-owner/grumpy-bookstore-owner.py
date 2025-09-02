0287b919bd9b929df31a6c0650d7f1c1a2471369
Abdulrahman Kolawole <backendkolawole@gmail.com>
2025-09-02 14:51:03 +0100

    Time: 17 ms (61.94%) | Memory: 19.2 MB (92.96%) - LeetSync

--- 1138-grumpy-bookstore-owner/grumpy-bookstore-owner.py
+++ 1138-grumpy-bookstore-owner/grumpy-bookstore-owner.py
@@ -1,23 +1,25 @@
 class Solution:
     def maxSatisfied(self, customers: List[int], grumpy: List[int], minutes: int) -> int:
-        i = 0
         j = 0
-        window = 0
-        max_window = 0
+        i = 0
         satisfied = 0
+        max_window = 0
+        window = 0
 
         while (j < len(customers)):
-            if (grumpy[j]):
-                window += customers[j]
-            else:
+            if (grumpy[j] == 0):
                 satisfied += customers[j]
+            else:
+                window += customers[j]
 
             if (j - i + 1 > minutes):
                 if (grumpy[i]):
                     window -= customers[i]
                 i += 1
+
             max_window = max(window, max_window)
-
             j += 1
+            
+        return max_window + satisfied
 
-        return satisfied + max_window
+        
