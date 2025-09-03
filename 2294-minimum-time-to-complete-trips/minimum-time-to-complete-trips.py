0204726920995f0b4425653d616c1da7419f70f1
Abdulrahman Kolawole <backendkolawole@gmail.com>
2025-09-03 11:50:25 +0100

    Time: 1107 ms (27.66%) | Memory: 30.3 MB (88.21%) - LeetSync

--- 2294-minimum-time-to-complete-trips/minimum-time-to-complete-trips.py
+++ 2294-minimum-time-to-complete-trips/minimum-time-to-complete-trips.py
@@ -2,17 +2,14 @@ class Solution:
     def minimumTime(self, time: List[int], totalTrips: int) -> int:
         i = 1
         j = max(time) * totalTrips
-        # time.sort()
 
         def canTrip(mid):
-            trips_completed = 0
-
+            completed_trips = 0
             for t in time:
-                trips_completed += math.floor(mid / t)
+                completed_trips += (mid // t)
 
-            return trips_completed >= totalTrips
-
-
+            return completed_trips >= totalTrips
+                
         while (i <= j):
             mid = (i + j) // 2
 
@@ -22,4 +19,5 @@ class Solution:
                 i = mid + 1
 
         return i
+
         
