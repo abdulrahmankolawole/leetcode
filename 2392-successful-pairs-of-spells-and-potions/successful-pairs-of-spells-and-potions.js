35e4d756fd5193c146b8d1fe148c0fd921e5dd4d
Abdulrahman Kolawole <backendkolawole@gmail.com>
2025-09-03 12:06:18 +0100

    Time: 119 ms (49.44%) | Memory: 86 MB (76.58%) - LeetSync

--- 2392-successful-pairs-of-spells-and-potions/successful-pairs-of-spells-and-potions.js
+++ 2392-successful-pairs-of-spells-and-potions/successful-pairs-of-spells-and-potions.js
@@ -1,23 +1,26 @@
 var successfulPairs = function(spells, potions, success) {
-    potions.sort((a, b) => a - b)
-    let size = spells.length
-    let output = new Array(size)
+    let output = []
+    potions.sort((a, b)=> a - b)
 
-    function bs(spell) {
+    function binarySearch(spell) {
         let i = 0
         let j = potions.length - 1
 
         while (i <= j) {
-            let mid = Math.floor((i + j)/ 2)
-            if (potions[mid] * spell >= success) j = mid - 1
+            let mid = Math.floor((i + j) / 2)
+            if (potions[mid] * spell >= success) {
+                j = mid - 1
+            }
             else i = mid + 1
         }
-        return potions.length - i
+        return i
     }
 
-    for (let i = 0; i < spells.length; i++) {
-        output[i] = bs(spells[i])
+    for (let i = 0; i < spells.length; i++){
+        let spell = spells[i]
+        let index = binarySearch(spell)
+        output[i] = potions.length - index
     }
+
     return output
-
 };
