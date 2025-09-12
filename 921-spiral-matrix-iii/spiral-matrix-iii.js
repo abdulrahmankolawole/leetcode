902cd767fcd1fb05e6c7d996fec604a38ee14bdf
Abdulrahman Kolawole <backendkolawole@gmail.com>
2025-09-12 14:16:24 +0100

    Time: 9 ms (6.25%) | Memory: 65.2 MB (39.58%) - LeetSync

--- 921-spiral-matrix-iii/spiral-matrix-iii.js
+++ 921-spiral-matrix-iii/spiral-matrix-iii.js
@@ -1,31 +1,31 @@
 var spiralMatrixIII = function(rows, cols, rStart, cStart) {
     let size = rows * cols
-    let result = new Array()   
+    let result = []
     let i = rStart
-    let j = cStart 
+    let j = cStart
     let steps = 1
-    let directions = [[0, 1], [1, 0], [0, -1], [-1, 0]]
+    let directions = [[0, 1], [1,0], [0, -1], [-1, 0]]
     let k = 0
 
-    function isValid(i, j) {
-        if (i < 0 || j < 0 || i >= rows || j >= cols) return false
+    function isValid(row, col) {
+        if (row < 0 || col < 0 || row >= rows || col >= cols) return false
         return true
     }
 
     while (result.length < size) {
         for (let a = 0; a < 2; a++) {
             let [x, y] = directions[k]
-            for (let b = 0; b < steps; b++) {
+            for (let step = 0; step < steps; step++) {
                 if (isValid(i, j)) {
                     result.push([i, j])
                 }
-                i = x + i
-                j = y + j
+                i = i + x
+                j = j + y
             }
             k = (k + 1) % 4
         }
-        steps +=1
-    } 
+        steps += 1
+    }
 
     return result
 };
