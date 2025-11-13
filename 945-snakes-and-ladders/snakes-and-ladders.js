7673bde0a256f6278a3d5a72495e5939d5e5337d
Abdulrahman Kolawole <backendkolawole@gmail.com>
2025-11-13 16:37:21 +0100

    Time: 9 ms (76.92%) | Memory: 60.5 MB (34.97%) - LeetSync

--- 945-snakes-and-ladders/snakes-and-ladders.js
+++ 945-snakes-and-ladders/snakes-and-ladders.js
@@ -1,43 +1,46 @@
 var snakesAndLadders = function(board) {
-    let n = board.length;
-    let coordinates = new Array((n * n) + 1);
-    let label = 1;
+    let n = board.length
+    let cells = new Array((n * n) + 1) 
+    let label = 1
 
     for (let i = n - 1; i >= 0; i--) {
         if ((n - i) % 2 == 1) {
-            for (let j = 0; j < n; j++) {
-                coordinates[label] = [i, j];
-                label += 1;
+            for (j = 0; j < n; j++) {
+                cells[label] = [i, j]
+                label += 1
             }
-        } else {
-            for (let j = n - 1; j >= 0; j--) {
-                coordinates[label] = [i, j];
-                label += 1;
+        }
+        else {
+            for (j = n - 1; j >= 0; j--) {
+                cells[label] = [i, j]
+                label += 1
             }
         }
     }
 
-    let minMoves = new Array((n * n) + 1).fill(-1);
-    minMoves[1] = 0;
-    let queue = [1];
-
+    let min_moves = new Array((n * n) + 1).fill(-1)
+    min_moves[1] = 0
+    let queue = [1]
     while (queue.length) {
-        let curr = queue.shift();
+        let current = queue.shift()
+        if (current === (n * n)) return min_moves[current]
 
-        if (curr == (n * n)) return minMoves[curr];
-
-        for (let next = curr + 1; next <= Math.min(curr + 6, n * n); next++) {
-            let [i, j] = coordinates[next];
-            let destination = next;
-            if (board[i][j] !== -1) {
-                destination = board[i][j];
+        for (let next = current + 1; next <= Math.min(current + 6, (n * n)); next++) {
+            let [i, j] = cells[next]
+            let destination = next
+            if (board[i][j] != -1) {
+                destination = board[i][j]
             }
-            if (minMoves[destination] == -1) {
-                queue.push(destination);
-                minMoves[destination] = minMoves[curr] + 1;
+
+            if (min_moves[destination] == -1) {
+                queue.push(destination)
+                min_moves[destination] = min_moves[current] + 1
             }
         }
     }
 
-    return -1;
+    return -1
+
+
+
 };
