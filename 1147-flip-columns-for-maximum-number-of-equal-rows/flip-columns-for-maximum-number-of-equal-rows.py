ea8c9979529a9086aa9b8e8790321a73f709836f
Abdulrahman Kolawole <backendkolawole@gmail.com>
2025-11-14 16:41:00 +0100

    Time: 35 ms (83.17%) | Memory: 20.8 MB (56.93%) - LeetSync

--- 1147-flip-columns-for-maximum-number-of-equal-rows/flip-columns-for-maximum-number-of-equal-rows.py
+++ 1147-flip-columns-for-maximum-number-of-equal-rows/flip-columns-for-maximum-number-of-equal-rows.py
@@ -1,21 +1,21 @@
 class Solution:
     def maxEqualRowsAfterFlips(self, matrix: List[List[int]]) -> int:
-        rows = len(matrix)
-        columns = len(matrix[0])
         lookup = {}
-        max_number = 0
+        rows = len(matrix)
+        columns = len(matrix)
+        count = 0
 
         for row in matrix:
-            if (row[0]):
-                key = tuple([0 if n else 1 for n in row])
-            else:
-                key = tuple(row)
+            row_key = tuple(row)
+            if (not row[0]):
+                row_key = tuple([0 if char == 1 else 1 for char in row])
             
-            if (key not in lookup):
-                lookup[key] = 0
-            lookup[key] += 1
-            max_number = max(max_number, lookup[key])
+            if (row_key not in lookup):
+                lookup[row_key] = 0
+            lookup[row_key] += 1
 
-        
-        return max_number
-        # return max(lookup.values())
+        for key in lookup:
+            if (lookup[key] > 1):
+                count = max(count, lookup[key])
+
+        return 1 if count == 0 else count
