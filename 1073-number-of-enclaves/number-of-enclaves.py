93be5e9c7498752c2169b47be15958715961ea16
Abdulrahman Kolawole <backendkolawole@gmail.com>
2025-11-14 15:14:18 +0100

    Time: 99 ms (41.51%) | Memory: 29.4 MB (62.11%) - LeetSync

--- 1073-number-of-enclaves/number-of-enclaves.py
+++ 1073-number-of-enclaves/number-of-enclaves.py
@@ -1,36 +1,37 @@
 class Solution:
     def numEnclaves(self, grid: List[List[int]]) -> int:
-
-        # iterate through the grid
-        # if the current cell is on the boundary
-        # traverse the adjacent land cells
-        # iterate through the grid and count the number of land cells remaining
-
         rows = len(grid)
         columns = len(grid[0])
+        output = 0
+
+        def isValid(i, j):
+            if (i < 0 or j < 0 or i == rows or j == columns):
+                return False
+            return True
 
         def traverse(i, j):
-            if (i < 0 or j < 0 or i == rows or j == columns or grid[i][j] != 1):
-                return 
+            if (not isValid(i, j) or grid[i][j] != 1):
+                return
             grid[i][j] = 0
-            traverse(i - 1, j) #up
-            traverse(i, j + 1) # right
-            traverse(i + 1, j) #down
-            traverse(i, j - 1) # left
+            traverse(i - 1, j)
+            traverse(i, j + 1)
+            traverse(i + 1, j)
+            traverse(i, j - 1)
 
         for i in range(rows):
             for j in range(columns):
-                if ((i == 0 or j == 0 or i == rows - 1 or j == columns - 1) and grid[i][j]):
-                    traverse(i, j)
+                cell = grid[i][j]
+                if (cell == 1):
+                    if ((i == 0 or i == rows - 1) or (j == 0 or j == columns - 1)):
+                        traverse(i, j)
 
-        count = 0
         for i in range(rows):
             for j in range(columns):
-                if (grid[i][j]):
-                    count += 1
+                cell = grid[i][j]
 
-        return count
-        
+                if (cell == 1):
+                    output += 1
 
 
+        return output
         
