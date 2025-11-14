7046a698040f961c0d67b9e037c4b0dd83ccb9d6
Abdulrahman Kolawole <backendkolawole@gmail.com>
2025-11-14 18:05:41 +0100

    Time: 22 ms (34.48%) | Memory: 18.4 MB (72.41%) - LeetSync

--- 1380-number-of-closed-islands/number-of-closed-islands.py
+++ 1380-number-of-closed-islands/number-of-closed-islands.py
@@ -2,32 +2,37 @@ class Solution:
     def closedIsland(self, grid: List[List[int]]) -> int:
         rows = len(grid)
         columns = len(grid[0])
+        output = 0
+
+        def isValid(i, j):
+            if (i < 0 or j < 0 or i == rows or j == columns or grid[i][j]):
+                return False
+            return True 
 
         def traverse(i, j):
-            if (i < 0 or j < 0 or i == rows or j == columns or grid[i][j]):
+            if (not isValid(i, j)):
                 return 
             grid[i][j] = 1
-            traverse(i - 1, j) #up
-            traverse(i, j + 1) #right
-            traverse(i + 1, j) #down
-            traverse(i, j - 1) #left
+            traverse(i - 1, j)
+            traverse(i, j + 1)
+            traverse(i + 1, j)
+            traverse(i, j - 1)
 
         for i in range(rows):
             for j in range(columns):
                 cell = grid[i][j]
 
-                if (not cell):
+                if (cell == 0):
                     if (i == 0 or j == 0 or i == rows - 1 or j == columns - 1):
                         traverse(i, j)
-
-        count = 0
+        
         for i in range(rows):
             for j in range(columns):
                 cell = grid[i][j]
 
-                if (not cell):
-                    count += 1
+                if (cell == 0):
+                    output += 1
                     traverse(i, j)
-        
-        return count
+
+        return output
         
