8361ab5176f31d9aa1081be185bb9b520ee98c10
Abdulrahman Kolawole <backendkolawole@gmail.com>
2025-11-14 17:29:26 +0100

    Time: 43 ms (71.93%) | Memory: 65.5 MB (61.48%) - LeetSync

--- 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.js
+++ 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.js
@@ -1,39 +1,34 @@
 var shortestPathBinaryMatrix = function(grid) {
     let n = grid.length
-    let mySet = new Set()
-    let directions = [[-1, 0], [-1, 1], [0, 1], [1, 1], [1, 0], [1, -1], [0, -1], [-1, -1]];
+    let directions = [[-1, 0], [-1, 1], [0, 1], [1, 1], [1, 0], [1, -1], [0, -1], [-1, -1]]
+    let queue = [[0, 0]]
     if (grid[0][0] !== 0 || grid[n - 1][n - 1] !== 0) return -1;
-
-
+    let path = 1
 
     function isValid(i, j) {
-        if (i < 0 || j < 0 || i== n || j == n || grid[i][j] == 1) return false
+        if (i < 0 || j < 0 || i == n || j == n) return false
         return true
     }
 
-    let queue = [[0, 0]]
-    let distance = 1
     while (queue.length) {
         let size = queue.length
 
-        for (let i = 0; i < size; i++) {
-            
+        for (let k = 0; k < size; k++) {
             let [i, j] = queue.shift()
-            if (i == n - 1 && j == n-1) return distance
+            if (i == n - 1 && j == n - 1) return path
+
             for (let [x, y] of directions) {
                 let new_x = x + i
                 let new_y = y + j
 
-                if (isValid(new_x, new_y)) {
-                    queue.push([new_x, new_y])
+                if (isValid(new_x, new_y) && grid[new_x][new_y] == 0) {
                     grid[new_x][new_y] = 1
+                    queue.push([new_x, new_y])
                 }
             }
-
         }
-        distance += 1
+        path += 1
     }
 
     return -1
-
 };
