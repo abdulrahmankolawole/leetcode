0d6df2edf9c9c8209534281c10004803e59b4d0a
Abdulrahman Kolawole <backendkolawole@gmail.com>
2025-11-15 17:03:33 +0100

    Time: 179 ms (49.62%) | Memory: 22.2 MB (25.19%) - LeetSync

--- 1711-find-valid-matrix-given-row-and-column-sums/find-valid-matrix-given-row-and-column-sums.py
+++ 1711-find-valid-matrix-given-row-and-column-sums/find-valid-matrix-given-row-and-column-sums.py
@@ -2,25 +2,27 @@ class Solution:
     def restoreMatrix(self, rowSum: List[int], colSum: List[int]) -> List[List[int]]:
         rows = len(rowSum)
         columns = len(colSum)
-        cur_col_sum = 0
         matrix = [[0] * columns for _ in range(rows)]
-
+        
+        # validate the rowsum
         for i in range(rows):
             matrix[i][0] = rowSum[i]
 
+        # validate the columns
         for j in range(columns):
-            cur_col_sum = 0
-
+            col_sum = 0
             for i in range(rows):
-                cur_col_sum += matrix[i][j]
-
+                col_sum += matrix[i][j]
+            
             i = 0
-            while (cur_col_sum > colSum[j]):
-                diff = cur_col_sum - colSum[j]
-                max_shift = min(diff, matrix[i][j])
+            while (col_sum > colSum[j]):
+                difference = col_sum - colSum[j]
+                max_shift = min(matrix[i][j], difference)
                 matrix[i][j + 1] += max_shift
                 matrix[i][j] -= max_shift
-                cur_col_sum -= max_shift
-                i += 1
+                col_sum -= max_shift
+                i += 1 
 
         return matrix
+
+        
