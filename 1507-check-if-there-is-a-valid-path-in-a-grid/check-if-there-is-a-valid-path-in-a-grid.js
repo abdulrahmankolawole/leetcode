eff65d592f73f0ca86a2c89c4152237a748543b6
Abdulrahman Kolawole <backendkolawole@gmail.com>
2025-11-15 12:19:13 +0100

    Time: 202 ms (47.37%) | Memory: 79.6 MB (68.42%) - LeetSync

--- 1507-check-if-there-is-a-valid-path-in-a-grid/check-if-there-is-a-valid-path-in-a-grid.js
+++ 1507-check-if-there-is-a-valid-path-in-a-grid/check-if-there-is-a-valid-path-in-a-grid.js
@@ -1,55 +1,45 @@
 var hasValidPath = function(grid) {
-    const m = grid.length;
-    const n = grid[0].length;
-    const queue = [[0, 0]]; // Start from the top-left corner
-    const up = {};
-    const right = {};
-    const down = {};
-    const left = {};
+    let rows = grid.length;
+    let columns = grid[0].length;
+    let queue = [[0, 0]];
+    let up = {};
+    let right = {};
+    let down = {};
+    let left = {};
     let lookup = new Set();
 
-    [2, 5, 6].forEach(x => {
-        up[x] = new Set([2, 3, 4]);
-    });
+    [2, 5, 6].forEach(x=> {up[x] = new Set([2, 3, 4])});
+    [1, 4, 6].forEach(x=>{ right[x] = new Set([1, 3, 5])});
+    [2, 3, 4].forEach(x=> {down[x] = new Set([2, 5, 6])});
+    [1, 3, 5].forEach(x=> {left[x] = new Set([1, 4, 6])});
+    let directions = [[-1, 0, up], [0, 1, right], [1, 0, down], [0, -1, left]];
 
-    [1, 4, 6].forEach(x => {
-        right[x] = new Set([1, 3, 5]);
-    });
-
-    [2, 3, 4].forEach(x => {
-        down[x] = new Set([2, 5, 6]);
-    });
-
-    [1, 3, 5].forEach(x => {
-        left[x] = new Set([1, 4, 6]);
-    });
-
-    const directions = [[-1, 0, up], [0, 1, right], [1, 0, down], [0, -1, left]];
-
-    function isValid(dr, dc, i, j, direction) {
-        if (dr < 0 || dc < 0 || dr == m || dc == n) return false
-        if (lookup.has(dr + "," + dc)) return false
+    function isValid(new_x, new_y, i, j, direction) {
+        if (new_x < 0 || new_y < 0 || new_x == rows || new_y == columns) return false
         let street = grid[i][j]
-        let path = grid[dr][dc]
-        if (!(street in direction)) return false
-        if (!(direction[street].has(path))) return false
+        let path = grid[new_x][new_y]
+
+        if (!(street in direction) || !(direction[street].has(path))) return false
         return true
     }
 
     while (queue.length) {
         let [i, j] = queue.shift()
+        if (i == rows - 1 && j == columns - 1) return true
 
-        if (i == m - 1 && j == n - 1) return true
         for (let [x, y, direction] of directions) {
-            let newX = x + i
-            let newY = y + j
+            let new_x = x + i
+            let new_y = y + j
 
-            if (isValid(newX, newY, i, j, direction)){
-                queue.push([newX, newY])
-                lookup.add(newX + "," + newY)
+            if (isValid(new_x, new_y, i, j, direction) && !lookup.has(new_x + "," + new_y)) {
+                queue.push([new_x, new_y])
+                lookup.add(new_x + "," + new_y)
             }
         }
     }
-    return false
-};
 
+    return false
+
+
+
+};
