e4711be4d1bd558faa133ea0638a8f84cae1c0b0
Abdulrahman Kolawole <backendkolawole@gmail.com>
2025-12-01 16:06:30 +0100

    Time: 0 ms (100.00%) | Memory: 55.8 MB (5.07%) - LeetSync

--- 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.js
+++ 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.js
@@ -1,17 +1,16 @@
 var findMin = function(nums) {
+
     let i = 0
     let j = nums.length - 1
-    if (nums[i] <= nums[j]) return nums[i]
-    
+    if (nums.length == 1 || nums[i] <= nums[j]) return nums[i]
+
     while (i <= j) {
         let mid = Math.floor((i + j) / 2)
         let leftOfMid = nums[mid - 1]
         let rightOfMid = nums[mid + 1]
-
         if (leftOfMid > nums[mid]) return nums[mid]
         if (nums[mid] > rightOfMid) return rightOfMid
         if (nums[i] <= nums[mid]) i = mid + 1
-        else if (nums[mid] <= nums[j]) j = mid - 1
-    }   
-
+        else j = mid - 1
+    }
 };
