6596f56ec9fdfdecd6843bcddc40c64cfd800e79
Abdulrahman Kolawole <backendkolawole@gmail.com>
2025-12-02 11:57:40 +0100

    Time: 0 ms (100.00%) | Memory: 61.9 MB (53.50%) - LeetSync

--- 658-find-k-closest-elements/find-k-closest-elements.js
+++ 658-find-k-closest-elements/find-k-closest-elements.js
@@ -4,11 +4,14 @@ var findClosestElements = function(arr, k, x) {
 
     while (i < j) {
         let mid = Math.floor((i + j) / 2)
+
         if (x - arr[mid] > arr[mid + k] - x) {
             i = mid + 1
         }
         else j = mid
-    }
+    }   
 
     return arr.slice(i, i + k)
+
+
 };
