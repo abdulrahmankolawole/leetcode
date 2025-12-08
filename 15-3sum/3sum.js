aa94cea7c0c285b994e3a13004d8e2404d4ebf4f
Abdulrahman Kolawole <backendkolawole@gmail.com>
2025-12-08 14:38:48 +0100

    Time: 547 ms (6.34%) | Memory: 74 MB (8.02%) - LeetSync

--- 15-3sum/3sum.js
+++ 15-3sum/3sum.js
@@ -1,28 +1,27 @@
 var threeSum = function(nums) {
-    nums = nums.sort((a, b)=> a - b)
     let lookup = new Set()
-    let target = 0
     let output = []
+    nums.sort((a, b)=> a - b)
 
     for (let i = 0; i < nums.length - 2; i++) {
-        a = i + 1
-        b = nums.length - 1
+        let j = i + 1
+        let k = nums.length - 1
 
-        while (a < b) {
-            sum = nums[i] + nums[a] + nums[b]
-            if (sum == target) {
-                triplet = nums[i] + ',' + nums[a] + ',' + nums[b]
-                if (!lookup.has(triplet)) {
-                    lookup.add(triplet)
-                    output.push([nums[i], nums[a], nums[b]])
-                }
-                a += 1
-                b -= 1
-            }
-            else if (sum < target) a +=1
-            else b -= 1
+        while (j < k) {
+            let sum = nums[i] + nums[j] + nums[k]
+            let triplets = nums[i] + "," + nums[j] + "," + nums[k]
+            if (sum == 0) {
+                if (!lookup.has(triplets)) {
+                    lookup.add(triplets)
+                    output.push([nums[i], nums[j], nums[k]])
+                } 
+                j ++
+                k --
+            } 
+            else if (sum < 0) j ++
+            else k --
         }
-    }  
-    
+    }
+
     return output
 };
