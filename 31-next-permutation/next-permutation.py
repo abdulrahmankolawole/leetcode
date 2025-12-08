5c3fd388bb2f16e9820bceb4d11ed4b129485d5b
Abdulrahman Kolawole <backendkolawole@gmail.com>
2025-12-08 17:09:00 +0100

    Time: 0 ms (100.00%) | Memory: 18 MB (13.85%) - LeetSync

--- 31-next-permutation/next-permutation.py
+++ 31-next-permutation/next-permutation.py
@@ -3,33 +3,33 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-
         break_index = -1
         for i in range(len(nums) - 2, -1, -1):
             if nums[i] < nums[i + 1]:
                 break_index = i
                 break 
-        
-        
-        if break_index == -1:
-            nums.reverse()
-            return
 
-        swap_index = -1
-        for i in range(len(nums) -1, break_index, -1):
-            if (nums[i] > nums[break_index]):
-                swap_index = i
-                break
+        if (break_index == -1):
+            nums.reverse()
+            return 
+
+        # swap_index = -1
+        # for i in range(len(nums) -1, break_index, -1):
+        #     if (nums[i] > nums[break_index]):
+        #         swap_index = i
+        #         break
         
-        nums[swap_index], nums[break_index] = nums[break_index], nums[swap_index]
+        # nums[swap_index], nums[break_index] = nums[break_index], nums[swap_index]
+        for i in range(len(nums) -1, break_index -1, -1):
+            if (nums[i] > nums[break_index]):
+                nums[break_index], nums[i] = nums[i], nums[break_index]
+                break
 
         i = break_index + 1
         j = len(nums) - 1
+
         while (i < j):
             nums[i], nums[j] = nums[j], nums[i]
             i += 1
             j -= 1
-        
-        # return nums
 
-        
