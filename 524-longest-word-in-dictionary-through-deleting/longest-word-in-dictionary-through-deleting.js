ea21eef041fb1f05c2005e127525b530438c8446
Abdulrahman Kolawole <backendkolawole@gmail.com>
2025-12-11 10:59:21 +0100

    Time: 58 ms (5.00%) | Memory: 64.5 MB (6.67%) - LeetSync

--- 524-longest-word-in-dictionary-through-deleting/longest-word-in-dictionary-through-deleting.js
+++ 524-longest-word-in-dictionary-through-deleting/longest-word-in-dictionary-through-deleting.js
@@ -1,31 +1,55 @@
-var findLongestWord = function(s, dictionary) {
+// var findLongestWord = function(s, dictionary) {
     
-    function isSubSeq(char, substr) {
+//     var isSubsequence = function(s, t) {
+//         let i = 0
+//         let j = 0
+
+//         while (j < t.length) {
+//             if (s[i] == t[j]) i += 1
+//             j += 1
+//         }   
+
+//         return i == s.length
+//     };
+
+//     let output = ""
+//     let maxLength = 0
+
+//     for (let word of dictionary) {
+//         if (isSubSeq(s, word)) {
+//             if (word.length > output.length) {
+//                 output = word
+//             }
+//             else if (word.length == output.length) {
+//                 output = output < word ? output : word
+//             }
+//         }
+//     }
+//     return output
+// };
+
+var findLongestWord = function(s, dictionary) {
+
+    dictionary.sort((a, b)=> b.length == a.length ? a.toLowerCase().localeCompare(b.toLowerCase()) : b.length - a.length)
+    console.log(dictionary)
+    
+    function isSubseq(s, t) {
         let i = 0
         let j = 0
 
-        while (i < char.length) {
-            if (char[i] == substr[j]) {
-                i += 1
-                j += 1
-            }
-            else i += 1
-        }
-        return j == substr.length
-    } 
+        while (j < t.length) {
+            if (s[i] == t[j]) i += 1
+            j += 1
+        }   
+
+        return i == s.length
+    };
 
     let output = ""
     let maxLength = 0
 
     for (let word of dictionary) {
-        if (isSubSeq(s, word)) {
-            if (word.length > output.length) {
-                output = word
-            }
-            else if (word.length == output.length) {
-                output = output < word ? output : word
-            }
-        }
+        if (isSubseq(word, s)) return word
     }
     return output
 };
