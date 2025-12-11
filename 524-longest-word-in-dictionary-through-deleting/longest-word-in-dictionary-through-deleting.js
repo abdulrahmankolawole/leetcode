96ec5956602752660328e340cfa3a944fc8a9ee9
Abdulrahman Kolawole <backendkolawole@gmail.com>
2025-12-11 11:02:06 +0100

    Time: 45 ms (5.00%) | Memory: 63.8 MB (8.33%) - LeetSync

--- 524-longest-word-in-dictionary-through-deleting/longest-word-in-dictionary-through-deleting.js
+++ 524-longest-word-in-dictionary-through-deleting/longest-word-in-dictionary-through-deleting.js
@@ -1,37 +1,6 @@
-// var findLongestWord = function(s, dictionary) {
-    
-//     var isSubsequence = function(s, t) {
-//         let i = 0
-//         let j = 0
-
-//         while (j < t.length) {
-//             if (s[i] == t[j]) i += 1
-//             j += 1
-//         }   
-
-//         return i == s.length
-//     };
-
-//     let output = ""
-//     let maxLength = 0
-
-//     for (let word of dictionary) {
-//         if (isSubSeq(s, word)) {
-//             if (word.length > output.length) {
-//                 output = word
-//             }
-//             else if (word.length == output.length) {
-//                 output = output < word ? output : word
-//             }
-//         }
-//     }
-//     return output
-// };
-
 var findLongestWord = function(s, dictionary) {
 
     dictionary.sort((a, b)=> b.length == a.length ? a.toLowerCase().localeCompare(b.toLowerCase()) : b.length - a.length)
-    console.log(dictionary)
     
     function isSubseq(s, t) {
         let i = 0
@@ -46,7 +15,6 @@ var findLongestWord = function(s, dictionary) {
     };
 
     let output = ""
-    let maxLength = 0
 
     for (let word of dictionary) {
         if (isSubseq(word, s)) return word
