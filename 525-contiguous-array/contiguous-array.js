cc70cad8f237f39c8b5cce89d1e38bd033021734
Abdulrahman Kolawole <backendkolawole@gmail.com>
2025-12-14 11:05:15 +0100

    Time: 39 ms (23.34%) | Memory: 65.1 MB (22.48%) - LeetSync

--- 525-contiguous-array/contiguous-array.js
+++ 525-contiguous-array/contiguous-array.js
@@ -1,17 +1,18 @@
 var findMaxLength = function(nums) {
-    let runningSum = 0
-    let output = 0
-    let lookup = {0: -1}
     let count = 0
+    let maxLength = 0
+    let lookup = {0: -1}
 
     for (let i = 0; i < nums.length; i++) {
-        if (nums[i] == 1) count += 1
-        else count -= 1
+        let num = nums[i]
+        if (num == 0) count -= 1
+        else if (num == 1) count += 1
         if (!(count in lookup)) lookup[count] = i
         else {
-            output = Math.max(output, i - lookup[count])
+            let length = i - lookup[count]
+            maxLength = Math.max(maxLength, length)
         }
     }   
-    return output
+    return maxLength
 
 };
