10f6c9b3db6589231b4de03b563cb5604f18eff1
Abdulrahman Kolawole <backendkolawole@gmail.com>
2025-12-14 09:16:45 +0100

    Time: 56 ms (28.70%) | Memory: 38.2 MB (32.89%) - LeetSync

--- 523-continuous-subarray-sum/continuous-subarray-sum.py
+++ 523-continuous-subarray-sum/continuous-subarray-sum.py
@@ -1,17 +1,23 @@
 class Solution:
     def checkSubarraySum(self, nums: List[int], k: int) -> bool:
+        # iterate through nums
+        # keep track of the curr_sum as we go
+        # check the current remainder
+        # if the current remainder has been seen return the size of the current subarray
+        
         lookup = {0: -1}
         curr_sum = 0
 
         for idx, val in enumerate(nums):
             curr_sum += val
             curr_rem = ((curr_sum % k) + k) % k
-            if (curr_rem not in lookup):
-                lookup[curr_rem] = idx
-            else:
-                if (idx - lookup[curr_rem] > 1):
+
+
+            if (curr_rem in lookup):
+                if (idx - lookup[curr_rem] >= 2):
                     return True
-        
-        return False
+            else:
+                lookup[curr_rem] = idx
 
         
+        return False
