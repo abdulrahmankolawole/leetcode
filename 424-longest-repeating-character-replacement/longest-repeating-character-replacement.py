024e2fa8c0902acb55d19be0f9f864441460a793
Abdulrahman Kolawole <backendkolawole@gmail.com>
2025-12-15 13:28:31 +0100

    Time: 123 ms (54.13%) | Memory: 18 MB (86.45%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
@@ -1,26 +1,25 @@
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
-        lookup = {}
         i = 0
         j = 0
-        output = 0
+        lookup = {}
         most_freq = 0
+        output = 0
 
         while (j < len(s)):
             char = s[j]
 
             if (char not in lookup):
-                lookup[char] = 1
-            else:
-                lookup[char] += 1
+                lookup[char] = 0
+            lookup[char] += 1
             most_freq = max(most_freq, lookup[char])
 
-            while (j - i + 1 - most_freq > k):
+            while ((j - i + 1) - most_freq > k):
                 lookup[s[i]] -= 1
                 i += 1
 
-            output = max(j - i + 1, output)
+            output = max(output, j - i + 1)
             j += 1
 
-
         return output
+        
