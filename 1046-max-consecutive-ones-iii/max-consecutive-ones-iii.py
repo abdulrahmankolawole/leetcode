3d30799bf45f6691a79d5af44093e285579bb8f3
Abdulrahman Kolawole <backendkolawole@gmail.com>
2025-12-15 14:59:05 +0100

    Time: 101 ms (13.33%) | Memory: 20.2 MB (98.05%) - LeetSync

--- 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.py
+++ 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.py
@@ -1,30 +1,23 @@
-# use a sliding window approach
-# declare two pointers i and j with initial value 0
-
 class Solution:
     def longestOnes(self, nums: List[int], k: int) -> int:
         i = 0
         j = 0
-        size = 0
-        count_of_zeroes = 0
-        
+        output = 0
+        zeroes = 0
+
         while (j < len(nums)):
-            
             if (nums[j] == 0):
-                count_of_zeroes += 1
-            
-            while (count_of_zeroes > k):
+                zeroes += 1
+
+            while (zeroes > k):
                 if (nums[i] == 0):
-                    count_of_zeroes -=1
-                i +=1
+                    zeroes -= 1
+                i += 1
 
-            size = max(size, j - i + 1)
-
+            output = max(output, j - i + 1)
             j += 1
 
-        return size
+        return output
 
 
-
-        
         
