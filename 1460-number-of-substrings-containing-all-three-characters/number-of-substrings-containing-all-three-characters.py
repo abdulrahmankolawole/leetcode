da3afb4534e263424570b0e6814fcd11e778d38e
Abdulrahman Kolawole <backendkolawole@gmail.com>
2025-12-16 14:06:10 +0100

    Time: 209 ms (11.80%) | Memory: 18 MB (37.94%) - LeetSync

--- 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.py
+++ 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.py
@@ -1,30 +1,32 @@
 class Solution:
     def numberOfSubstrings(self, s: str) -> int:
-        
+        lookup = {}
+        count = 0
+        i = 0
+        j = 0
+
         def hasAllThree():
-            
-            if (("a" not in lookup) or ("b" not in lookup) or ("c" not in lookup)):
+            if ("a" not in lookup or "b" not in lookup or "c" not in lookup):
                 return False
             if (lookup["a"] < 1 or lookup["b"] < 1 or lookup["c"] < 1):
                 return False
             return True
 
-        i = 0
-        j = 0
-        output = 0
-        lookup = {}
-
         while (j < len(s)):
             char = s[j]
+
             if (char not in lookup):
                 lookup[char] = 0
             lookup[char] += 1
 
             while (hasAllThree()):
-                output += len(s) - j
-                lookup[s[i]] -=1
+                count += len(s) - j
+                lookup[s[i]] -= 1
                 i += 1
+
 
             j += 1
 
-        return output
+        return count
+
+        
