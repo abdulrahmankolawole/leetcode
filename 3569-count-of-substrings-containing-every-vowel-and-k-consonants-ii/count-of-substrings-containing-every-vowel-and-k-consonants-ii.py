fffd949dc798daf8dd50cb22289e7e8f0c616232
Abdulrahman Kolawole <backendkolawole@gmail.com>
2025-12-17 13:39:17 +0100

    Time: 2131 ms (31.43%) | Memory: 18.7 MB (63.57%) - LeetSync

--- 3569-count-of-substrings-containing-every-vowel-and-k-consonants-ii/count-of-substrings-containing-every-vowel-and-k-consonants-ii.py
+++ 3569-count-of-substrings-containing-every-vowel-and-k-consonants-ii/count-of-substrings-containing-every-vowel-and-k-consonants-ii.py
@@ -2,28 +2,28 @@ class Solution:
     def countOfSubstrings(self, word: str, k: int) -> int:
 
         def atLeastK(k):
-            vowels = defaultdict(int)
+            lookup = defaultdict(int)
             consonants = 0
             output = 0
             j = 0
             i = 0
-            lookup = set("aeiou")
+            vowels = set("aeiou")
 
             while (j < len(word)):
-                if (word[j] in lookup):
-                    vowels[word[j]] += 1
+                if (word[j] in vowels):
+                    lookup[word[j]] += 1
                 else:
                     consonants += 1
 
-                while (len(vowels) == 5 and consonants >= k):
+                while (len(lookup) == 5 and consonants >= k):
                     output += len(word) - j
 
-                    if (word[i] in lookup):
-                        vowels[word[i]] -= 1
+                    if (word[i] in vowels):
+                        lookup[word[i]] -= 1
                     else:
                         consonants -= 1
-                    if (vowels[word[i]] == 0):
-                        vowels.pop(word[i])
+                    if (lookup[word[i]] == 0):
+                        lookup.pop(word[i])
                     i += 1
 
                 j += 1
