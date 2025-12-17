7b0c438ef592c2fb5290b486bc76cf9d375de85a
Abdulrahman Kolawole <backendkolawole@gmail.com>
2025-12-17 12:48:08 +0100

    Time: 267 ms (40.26%) | Memory: 18.2 MB (52.81%) - LeetSync

--- 2599-take-k-of-each-character-from-left-and-right/take-k-of-each-character-from-left-and-right.py
+++ 2599-take-k-of-each-character-from-left-and-right/take-k-of-each-character-from-left-and-right.py
@@ -2,8 +2,8 @@ from collections import Counter
 
 class Solution:
     def takeCharacters(self, s: str, k: int) -> int:
-        if k == 0:
-            return 0
+        # if k == 0:
+        #     return 0
         
         n = len(s)
         count = Counter(s)
@@ -14,22 +14,24 @@ class Solution:
         
         # We will use sliding window to find the longest middle substring
         # where the characters outside (taken from ends) still satisfy >= k of each
-        curr = {'a': 0, 'b': 0, 'c': 0}
-        left = 0
-        max_middle = 0
+        lookup = {'a': 0, 'b': 0, 'c': 0}
+        i = 0
+        j = 0
+        output = 0
         
-        for right in range(n):
-            curr[s[right]] += 1
+        while j < len(s):
+            lookup[s[j]] += 1
             
-            # Shrink from left while the current window (middle) makes any char taken < k
-            while (count['a'] - curr['a'] < k or
-                   count['b'] - curr['b'] < k or
-                   count['c'] - curr['c'] < k):
-                curr[s[left]] -= 1
-                left += 1
+            # Shrink from i while the current window (middle) makes any char taken < k
+            while (count['a'] - lookup['a'] < k or
+                   count['b'] - lookup['b'] < k or
+                   count['c'] - lookup['c'] < k):
+                lookup[s[i]] -= 1
+                i += 1
             
-            # After shrinking, the current window [left..right] is valid middle
-            max_middle = max(max_middle, right - left + 1)
+            # After shrinking, the lookupent window [i..j] is valid middle
+            output = max(output, j - i + 1)
+            j += 1
         
         # Minimum characters taken = total length - longest valid middle
-        return n - max_middle
+        return n - output
