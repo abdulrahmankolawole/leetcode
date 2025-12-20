2a143561c6f5fecb6dd9ce6a2b41227fb949c26e
Abdulrahman Kolawole <backendkolawole@gmail.com>
2025-12-20 12:42:28 +0100

    Time: 6 ms (14.04%) | Memory: 61.7 MB (78.42%) - LeetSync

--- 1538-maximum-points-you-can-obtain-from-cards/maximum-points-you-can-obtain-from-cards.js
+++ 1538-maximum-points-you-can-obtain-from-cards/maximum-points-you-can-obtain-from-cards.js
@@ -1,21 +1,21 @@
 var maxScore = function(cardPoints, k) {
-    let score = 0
-    let maxScore = 0
-    let totalScore = cardPoints.reduce((a, b)=> a + b)
-    let windowSum = 0
+    let total = cardPoints.reduce((a, b)=> a + b, 0)
     let windowSize = cardPoints.length - k
+    let windowSum = 0
+    minWindowSum = Infinity
+    let output = 0
 
     for (let i = 0; i < windowSize; i++) {
         windowSum += cardPoints[i]
-    }   
-
-    maxScore = totalScore - windowSum
-    for (let i = windowSize; i < cardPoints.length; i++) {
-        windowSum += cardPoints[i] - cardPoints[i - windowSize]
-        score = totalScore - windowSum
-        maxScore = Math.max(score, maxScore)
     }
 
-    return maxScore
+    output = total - windowSum
+
+    for (let i = windowSize; i < cardPoints.length; i++) {
+        windowSum += cardPoints[i] - cardPoints[i - windowSize]
+        minWindowSum = Math.min(windowSum, minWindowSum)
+    }
+    
+    return Math.max(total - minWindowSum, output)
 
 };
