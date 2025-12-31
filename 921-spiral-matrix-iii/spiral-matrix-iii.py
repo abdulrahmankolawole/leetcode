94f594d752d90bb1d21df1f4b5d94baa011d47b4
Abdulrahman Kolawole <backendkolawole@gmail.com>
2025-12-31 08:56:15 +0100

    Time: 14 ms (37.56%) | Memory: 18.7 MB (88.34%) - LeetSync

--- 921-spiral-matrix-iii/spiral-matrix-iii.py
+++ 921-spiral-matrix-iii/spiral-matrix-iii.py
@@ -1,35 +1,33 @@
 class Solution:
     def spiralMatrixIII(self, rows: int, cols: int, rStart: int, cStart: int) -> List[List[int]]:
-        r = rStart
-        c = cStart
+        output = []
+        directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+        i = rStart
+        j = cStart
+        steps = 1
+        k = 0
 
         def isValid(i, j):
-            if (i < 0 or j < 0 or i >= rows or j >= cols):
+            if ((i < 0 or i >= rows) or (j < 0 or j >= cols)):
                 return False
             return True
-
-        result = []
-        directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
-        steps = 1
-        i = 0
-
-        while (len(result) < rows * cols):
-
+        
+        while (len(output) < rows * cols):
+                
             for _ in range(2):
-                a, b = directions[i]
-                for _ in range(steps):
-                    if (isValid(r, c)):
-                        result.append([r, c])
-                
-                    r += a
-                    c += b
+                direction = directions[k]
+                x, y = direction
 
+                for _ in range(steps):
+
+                    if (isValid(i, j)):
+                        output.append([i, j]) 
+                    i = x + i
+                    j = j + y
+
+                k = (k + 1) % len(directions)
                 
-                i = (i + 1) % 4
             steps += 1
 
-        return result
-
-        
-
+        return output
         
