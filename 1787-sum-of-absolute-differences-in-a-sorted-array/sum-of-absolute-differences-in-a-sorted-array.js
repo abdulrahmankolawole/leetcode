f7594cf8f794ca0b975cfd8b895208ff3371b250
Abdulrahman Kolawole <backendkolawole@gmail.com>
2026-03-02 15:32:07 +0100

    Time: 153 ms (87.18%) | Memory: 79.5 MB (30.77%) - LeetSync

--- 1787-sum-of-absolute-differences-in-a-sorted-array/sum-of-absolute-differences-in-a-sorted-array.js
+++ 1787-sum-of-absolute-differences-in-a-sorted-array/sum-of-absolute-differences-in-a-sorted-array.js
@@ -1,14 +1,16 @@
 var getSumAbsoluteDifferences = function(nums) {
-    let total = nums.reduce((a, b)=> a + b, 0)
-    let output = new Array(nums.length).fill(0)
+    let totalSum = nums.reduce((a, b)=> a + b, 0)
     let leftSum = 0
+    let currentSum = 0
+    let output = new Array(nums.length).fill(0)
 
     for (let i = 0; i < nums.length; i++) {
         let leftSize = i
-        let rightSize = nums.length - leftSize - 1
-        rightSum = total - leftSum - nums[i]
-        output[i] = ((leftSize * nums[i]) - leftSum) + (rightSum - (nums[i] * rightSize)) 
+        rightSize = nums.length - i - 1
+        let rightSum = totalSum - leftSum - nums[i]
+        output[i] = ((leftSize * nums[i]) - leftSum) + (rightSum - (rightSize * nums[i]))
         leftSum += nums[i]
     }
-    return output  
+
+    return output
 };
