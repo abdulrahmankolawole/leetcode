d84b59fb4d8bed52e8aa0171703ab7dce813218c
Abdulrahman Kolawole <backendkolawole@gmail.com>
2026-03-03 10:04:13 +0100

    Time: 131 ms (7.29%) | Memory: 33.1 MB (53.65%) - LeetSync

---
+++ 1620-check-if-array-pairs-are-divisible-by-k/check-if-array-pairs-are-divisible-by-k.py
@@ -0,0 +1,25 @@
+class Solution:
+    def canArrange(self, arr: List[int], k: int) -> bool:
+        lookup = {}
+
+        for num in arr:
+            rem = ((num % k) + k) % k
+
+            if (rem not in lookup):
+                lookup[rem] = 0
+            lookup[rem] += 1
+
+        if (0 in lookup):
+            if (lookup[0] % 2 != 0):
+                return False
+        
+        for i in range(1, k):
+            if ((i + k) % k not in lookup):
+                continue
+            if (((i + k) % k in lookup and (k-i + k) % k not in lookup)):
+                return False
+            if ((lookup[(i + k) % k] != lookup[(k - i + k) % k])):
+                return False
+
+        return True
+        
