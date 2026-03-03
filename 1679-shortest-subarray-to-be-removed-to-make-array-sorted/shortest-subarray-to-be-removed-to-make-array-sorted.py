f3c75d9bbeba2e4ac05faca8d989784ccc9dd691
Abdulrahman Kolawole <backendkolawole@gmail.com>
2026-03-03 17:51:40 +0100

    Time: 18 ms (29.41%) | Memory: 34.1 MB (70.31%) - LeetSync

--- 1679-shortest-subarray-to-be-removed-to-make-array-sorted/shortest-subarray-to-be-removed-to-make-array-sorted.py
+++ 1679-shortest-subarray-to-be-removed-to-make-array-sorted/shortest-subarray-to-be-removed-to-make-array-sorted.py
@@ -1,32 +1,30 @@
 class Solution:
     def findLengthOfShortestSubarray(self, arr: List[int]) -> int:
-
-        # find the length of minimum postfix that can be removed
-        i = 0
         N = len(arr)
+        i = 0
         j = N - 1
         output = N
 
-        while (i < N - 1 and arr[i] <= arr[i + 1]):
+        while (i < N -1 and arr[i] <= arr[i + 1]):
             i += 1
 
-        # find the length of minimum prefix that can be removed
         while (j > 0 and arr[j] >= arr[j - 1]):
-            j -= 1
+            j -=1 
 
         if (j == 0):
             return 0
 
+        output = min(output, j, N - i - 1)
         l = 0
         r = j
-        output = min(output, N - i - 1, r)
 
         while (l <= i and r < N):
             if (arr[l] <= arr[r]):
-                output = min(output, r - l - 1)
+                output = min(output, r- l - 1)
                 l += 1
             else:
                 r += 1
 
-        return output
+        return output 
+        
         
