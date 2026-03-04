332ebce796f3f8377b319bd88fab2790202275c8
Abdulrahman Kolawole <backendkolawole@gmail.com>
2026-03-04 14:03:43 +0100

    Time: 0 ms (100.00%) | Memory: 19.3 MB (86.51%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.py
+++ 496-next-greater-element-i/next-greater-element-i.py
@@ -7,17 +7,20 @@ class Solution:
         for idx, val in enumerate(nums1):
             lookup[val] = idx
 
-        for i in range(len(nums2) -1, -1, -1):
-            if (not stack):
-                stack.append(nums2[i])
-            else:
-                while (stack and nums2[i] >= stack[-1]):
-                    stack.pop()
-                if (stack and nums2[i] in lookup):
-                    idx = lookup[nums2[i]]
-                    result[idx] = stack[-1]
-                stack.append(nums2[i])
+        j = len(nums2) - 1
 
+        while (j >= 0):
+            if (not stack):
+                stack.append(nums2[j])
+            else:
+                while (stack and nums2[j] >= stack[-1]):
+                    stack.pop()
+                if (stack and nums2[j] in lookup):
+                    idx = lookup[nums2[j]]
+                    result[idx] = stack[-1]
+                stack.append(nums2[j])
+
+            j -= 1
 
         return result
 
