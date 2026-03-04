62150d031767f68f11fe81db71f16fa636128bc4
Abdulrahman Kolawole <backendkolawole@gmail.com>
2026-03-04 12:27:03 +0100

    Time: 1 ms (46.67%) | Memory: 53.4 MB (80.00%) - LeetSync

--- 1923-sentence-similarity-iii/sentence-similarity-iii.js
+++ 1923-sentence-similarity-iii/sentence-similarity-iii.js
@@ -1,22 +1,24 @@
 var areSentencesSimilar = function(sentence1, sentence2) {
     let arr1 = sentence1.split(" ")
-    let arr2 = sentence2.split(" ")    
+    let arr2 = sentence2.split(" ")
 
-    if (arr2.length < arr1.length) {
+    if (arr1.length > arr2.length) {
         [arr1, arr2] = [arr2, arr1]
     }
 
-    i = 0
-    j = arr1.length - 1
-    r = arr2.length - 1
+    let i = 0
+    let j = arr1.length
+    let k = arr2.length
 
-   
-    while (i < arr1.length && arr1[i] == arr2[i]) i += 1
+    while (i < arr1.length && arr1[i] == arr2[i]) {
+        i += 1
+    }
 
-    while (j >= 0 && r >= 0 && arr1[j] == arr2[r]) {
+    while (j >= 0 && k >= 0 && arr1[j] == arr2[k]) {
         j -= 1
-        r -= 1
+        k -= 1
     }
 
     return i > j
+
 };
