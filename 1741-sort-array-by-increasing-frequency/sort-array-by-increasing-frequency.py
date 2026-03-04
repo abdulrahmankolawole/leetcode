402a33e6d06b3897addb881127032af8a7ee3c37
Abdulrahman Kolawole <backendkolawole@gmail.com>
2026-03-04 16:16:29 +0100

    Time: 0 ms (100.00%) | Memory: 19.3 MB (74.58%) - LeetSync

--- 1741-sort-array-by-increasing-frequency/sort-array-by-increasing-frequency.py
+++ 1741-sort-array-by-increasing-frequency/sort-array-by-increasing-frequency.py
@@ -1,27 +1,7 @@
 class Solution:
     def frequencySort(self, nums: List[int]) -> List[int]:
-        output = []
-        counter = Counter(nums)
-        buckets = {}
-        n = len(nums)
+        lookup = Counter(nums)
 
-        for num, freq in counter.items():
-            if (freq not in buckets):
-                buckets[freq] = []
-            buckets[freq].append(num)
-
-        print(buckets)
-                
-        for freq in range(1, n + 1):
-            if (freq in buckets):
-                numbers = buckets[freq]
-
-                numbers.sort(reverse=True)
-
-                print("numbers: ", numbers)
-
-                for num in numbers:
-                    for _ in range(freq):
-                        output.append(num)
-
-        return output   
+        nums.sort(key = lambda x: (lookup[x], -x))
+        return nums
+        
